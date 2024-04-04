0f1d575f79521707fb586e66fae22e00275ec04a
shivaniMinskole <shivaniminskole27@gmail.com>
2024-04-04 14:25:20 +0530

    intro of js

---
+++ basic_introduction.js
@@ -0,0 +1,43 @@
+// # js History:
+// -js invented by Sir Brenden Eich in 1995 for Netscape2.
+// -first name is livescript, after javascript.
+
+// -What is ECMASCRIPT?
+// -ECMAScript is a specification/standard.
+// -JavaScript is an implementation of the specification/standard.
+// -ECMAScript is standardized JavaScript.
+// -ECMAScript is JavaScript
+// -ECMA-Standard provider, javascript-Standard follower
+
+// -ES5, ES6?
+// -what is ES6? --> ES version/js version
+// ---------------------------------------------------------------
+
+// - What is JavaScript?
+// - Dynamically typed,synchronous,single threaded(1 call stack), versatile programing language
+// - versatile(used for clint and server side).
+
+// - only browser can understand the js. How ?
+// - js Engine 
+// default browser can read js.
+// js is browser language
+// browser have its own execution engine
+// E.g: Chrome --> v8
+
+// - Node ?
+// - node : can run the js outside of browser using node
+
+// setup:
+// node
+// vsc
+
+// online js code editor:
+// https://jsbin.com/?html,output
+// -----------------------------------------------------------------------
+
+// dynamicaly typed language
+let a //variable defind
+a = 10 // variable ko value asign kr rhe hai
+console.log(a) // get 10 output
+a = "shivani"
+console.log(a) // get update value of a which is shivani
